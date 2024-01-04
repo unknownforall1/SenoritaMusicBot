a629c96f0daca70fa9dde700a6c5e1951ba22017
Vivek Raj Roy <83235714+unknownforall1@users.noreply.github.com>
2024-01-05 05:00:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,6 @@ from pyrogram import filters
 load_dotenv()
 
 # Get this value from my.telegram.org/apps
-API_ID = int(getenv("API_ID"))
 API_ID = int(getenv("API_ID", "7988735"))
 API_HASH = getenv("API_HASH", "8339b7684eb7f4653ed032d4828ebf89")
 
