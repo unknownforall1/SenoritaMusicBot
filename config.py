a7f663bea89186806afefa006d219f5921355b38
Vivek Raj Roy <83235714+unknownforall1@users.noreply.github.com>
2024-01-06 13:10:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/saifalisew1508/SayuMusic",
+    "https://github.com/unknownforall1/missqt",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
